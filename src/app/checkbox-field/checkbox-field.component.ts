59f624de3457bd83e2be033e5d52dabf21dd11ae
Nika <white4ik1995@gmail.com>
2024-05-22 15:10:54 +0400

    add types and html for checkbox component

--- src/app/checkbox-field/checkbox-field.component.ts
+++ src/app/checkbox-field/checkbox-field.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox-field',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './checkbox-field.component.html',
   styleUrl: './checkbox-field.component.scss'
 })
 export class CheckboxFieldComponent {
-
+@Input() title!: string;
+@Input() items!: string[];
+@Input() control!: FormControl
 }
